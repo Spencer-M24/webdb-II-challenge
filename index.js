69493ef78b9b2c567e0ab3dd1e5fb68aa006debc
Spencer Meredith <Smeredith1119@gmail.com>
2019-03-26 05:50:43 -0700

    added knex/sqlite3 working on api crud

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+// Moduels IMports
+
+
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+
+
+
+
+
 
 const server = express();
 
@@ -7,6 +17,31 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+// CRud start
+const db = knex(knexConfig.development);
+
+
+server.get('/', (req, res) => {
+	res.send('Testing Test');
+});
+
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+
+
+
+
+
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
