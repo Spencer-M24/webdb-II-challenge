16654693b7ad6b77d18c18bc504baf97036c735d
Spencer Meredith <Smeredith1119@gmail.com>
2019-03-26 07:18:05 -0700

    finsihed mvp

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,18 @@
-modules.,exports = {
+modules.exports = {
 
+    development: {
+        
+        client: 'sqlite3',
+        
+        connection: {
+        
+            filename: './data/lambda.sqlite3'
+        },
+       
+        useNullAsDefault: true
+      },
     
+    
+
+
 }
